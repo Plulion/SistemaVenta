05a1e9a68614be05a833dc744f5a6c154b70b80e
Jose Ruz <jose.ruz.dba@gmail.com>
2021-07-13 19:57:44 -0400

    RRSS agregar funcionando

---
+++ src/DAO/ImplementRRSS.java
@@ -0,0 +1,99 @@
+package DAO;
+
+import Conexion.Conexion;
+import Modelo.Bancos;
+import Modelo.RRSS;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+
+public class ImplementRRSS implements IRRSS{
+
+    @Override
+    public ArrayList<RRSS> listarRRSS(String nombreRRSS) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean eliminarRRSS(String RRSS, int codigoRRSS) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean actualizarRRSS(String RRSS, int codigoRRSS, RRSS rrss) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean agregarRRSS(RRSS rrss) {       
+        
+        Conexion conexion = new Conexion();
+
+        try {
+            String sql = "INSERT INTO rrss (RRS_NOMBRE, CodigoRs, Activo) VALUES(?, ?, ?)";
+            PreparedStatement stmt = conexion.conectar().prepareStatement(sql);
+
+            stmt.setString(1, rrss.getRrsNombre());
+            stmt.setString(2, rrss.getCodigoRs());
+            stmt.setBoolean(3, rrss.isActivo());
+
+            stmt.executeUpdate();
+
+            return true;
+
+        } catch (Exception e) {
+            System.err.println("ERROR: " + e);
+        }
+        return false;
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void obtenerTodos(JTable tabla, DefaultTableModel model) {
+        
+        model.setRowCount(0);
+
+        Conexion conexion = new Conexion();
+
+        ArrayList<RRSS> list = new ArrayList<>();
+
+        try {
+
+            Statement smt = conexion.conectar().createStatement();
+
+            String sql = "SELECT * FROM rrss";
+
+            ResultSet rs = smt.executeQuery(sql);
+
+            while (rs.next()) {
+                list.add(new RRSS(
+                        rs.getInt("ID"),
+                        rs.getString("RRS_NOMBRE"),
+                        rs.getString("CodigoRS"),
+                        rs.getBoolean("Activo")
+                ));
+            }
+
+            Object[] row = new Object[4];
+
+            for (int i = 0; i < list.size(); i++) {
+                row[0] = list.get(i).getId();
+                row[1] = list.get(i).getRrsNombre();
+                row[2] = list.get(i).getCodigoRs();
+                row[3] = list.get(i).isActivo();
+
+                model.addRow(row);
+            }
+
+        } catch (Exception e) {
+            System.err.println("ERROR en:" + e);
+        }
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    
+}
---
+++ src/Modelo/RRSS.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ *
+ * @author Jose
+ */
+public class RRSS {
+    private int id;
+    private String rrsNombre;
+    private String codigoRs;
+    private boolean activo;
+
+    public RRSS() {
+    }
+
+    public RRSS(int id, String rrsNombre, String codigoRs, boolean activo) {
+        this.id = id;
+        this.rrsNombre = rrsNombre;
+        this.codigoRs = codigoRs;
+        this.activo = activo;
+    }
+
+    public RRSS(String rrsNombre, String codigoRs, boolean activo) {
+        this.rrsNombre = rrsNombre;
+        this.codigoRs = codigoRs;
+        this.activo = activo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getRrsNombre() {
+        return rrsNombre;
+    }
+
+    public void setRrsNombre(String rrsNombre) {
+        this.rrsNombre = rrsNombre;
+    }
+
+    public String getCodigoRs() {
+        return codigoRs;
+    }
+
+    public void setCodigoRs(String codigoRs) {
+        this.codigoRs = codigoRs;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+
+    @Override
+    public String toString() {
+        return "RRSS{" + "id=" + id + ", rrsNombre=" + rrsNombre + ", codigoRs=" + codigoRs + ", activo=" + activo + '}';
+    }
+
+   
+    
+    
+         
+}
